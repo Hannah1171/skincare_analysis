92716f4734a3cb7ea42f63a756a2da7ac868d809
Hannah Schlaucher <hanschlau@gmail.com>
2025-06-11 09:35:38 +0200

    inital trends

--- skincare/scripts/main.py
+++ skincare/scripts/main.py
@@ -1,10 +1,11 @@
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+from skincare.analysis.trends import get_trends
 from skincare.scripts.data_connection import load_comments_posts_transcript, load_posts_transcripts, load_hashtags_posts
 from skincare.pipeline.preprocessing import filter_by_language, detect_language, filter_by_date, filter_by_recent_days
 from skincare.analysis.sentiment import run_sentiment_analysis_on_comments
-from skincare.analysis.trends import get_trending_keywords_with_tfidf
+from skincare.analysis.trends_tdif import get_trending_keywords_with_tfidf
 from skincare.analysis.topics import generate_hierarchical_topics, run_topic_model
 import pandas as pd
 
@@ -28,10 +29,19 @@ def prepare_data(cache=True):
 if __name__ == "__main__":
     
     #prepare_data(cache=False)
-    df_comments = pd.read_csv('data/comments_sentiment_posts_transcripts.csv')
-    df_comments_recent = filter_by_recent_days(df=df_comments, days=30)
-    model, topic_summary, df_named = run_topic_model(df=df_comments_recent)
+    #df_comments = pd.read_csv('data/comments_sentiment_posts_transcripts.csv')
+    #df_comments_recent = filter_by_recent_days(df=df_comments, days=60)
+    #model, topic_summary, df_named = run_topic_model(df=df_comments_recent)
 
-    topic_summary.to_csv("data/topic_summary.csv", index=False)
-    df_named.to_csv("data/df_named.csv", index=False)
-    print(df_comments_recent['text'].nunique())
+    posts = pd.read_csv('data/posts_transcripts.csv')
+    posts = filter_by_recent_days(df=posts, days=90)
+
+    top_df, topics, trends = get_trends(df=posts, min_history=5)
+    top_df.to_csv("data/trend_top.csv")
+    trends.to_csv("data/trends.csv")
+    print(topics)
+
+
+    #topic_summary.to_csv("data/topic_summary.csv", index=False)
+    #df_named.to_csv("data/df_named.csv", index=False)
+    #print(df_comments_recent['text'].nunique())
