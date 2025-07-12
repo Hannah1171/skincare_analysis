509a102676c8a173ed2b453ef3f1f51db98fe6b4
ritusuhasshetkar <ritusuhasshetkar@gmail.com>
2025-07-12 21:51:36 +0200

    Final Code of Ritu Notebooks

--- backend/data_source/sql/transcripts.sql
+++
@@ -1,3 +0,0 @@
-SELECT id as post_id,detected_language,transcribed_text, video_description, bucketUrl
-
-  FROM `capstone-ai-dev.rawmart.tiktok_transcripts` 
--- backend/models/keywords.py
+++
@@ -1,2 +0,0 @@
-#top n grams for all combined text in total in a list
-
